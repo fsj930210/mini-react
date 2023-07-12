3f7c1569a92b5c147fd7aeff8f8f636f9a6ec5ed
fsj930210 <fsj930210@163.com>
2023-07-12 22:08:11 +0800

    feat: 实现jsx方法

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,50 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import { Type, Key, Ref, Props, ReactElementType, ElementType } from 'shared/ReactTypes';
+
+// ReactElement
+const ReactElement = function (type: Type, key: Key, ref: Ref, props: Props): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'custom_field'
+	};
+	return element;
+};
+
+// jsx
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any[]) => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
+	for (const prop in config) {
+		const value = config[prop];
+		if (prop === 'key') {
+			if (value !== undefined) {
+				key = '' + value;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (value !== undefined) {
+				ref = value;
+			}
+			continue;
+		}
+		if (Object.prototype.hasOwnProperty.call(config, prop)) {
+			props[prop] = value;
+		}
+		const maybeChildrenLength = maybeChildren.length;
+		if (maybeChildrenLength > 0) {
+			if (maybeChildrenLength === 1) {
+				props.children = maybeChildren[0];
+			} else {
+				props.children = maybeChildren;
+			}
+		}
+		return ReactElement(type, key, ref, props);
+	}
+};
+export const jsxDEV = jsx;
